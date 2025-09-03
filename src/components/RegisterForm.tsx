6c8a2c8daa6af9f5eee0392ec19ae628e08c80e6
arthurdagnon0-art <arthurdagnon0@gmail.com>
2025-09-03 16:10:15 +0100

    Updated RegisterForm.tsx

--- src/components/RegisterForm.tsx
+++ src/components/RegisterForm.tsx
@@ -135,7 +135,7 @@ export const RegisterForm: React.FC<RegisterFormProps> = ({ onRegister, onSwitch
               <button
                   type="button"
                   onClick={() => setVisible(!visible)}
-                  className="absolute right-2 top-1/2 -translate-y-1/2 text-gray-500 hover:text-gray-700"
+                  className="absolute right-4 top-20 -translate-y-1/2 text-gray-500 hover:text-gray-700"
                 >
                   {visible ? <EyeOff size={20} /> : <Eye size={20} />}
                 </button>
