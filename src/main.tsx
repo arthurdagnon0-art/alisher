c5304e64ee9fa4643a429ff36c11f0ecff2d6aed
arthurdagnon0-art <arthurdagnon0@gmail.com>
2025-09-04 18:04:48 +0100

    Fix React Hooks order error

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,14 @@
 import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
 import App from './App.tsx';
+import { AdminApp } from './components/admin/AdminApp';
 import './index.css';
+
+// Check if we're on the admin route
+const isAdminRoute = window.location.pathname.startsWith('/admin');
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <App />
+    {isAdminRoute ? <AdminApp /> : <App />}
   </StrictMode>
 );
